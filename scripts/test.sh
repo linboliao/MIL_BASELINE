03d4ef5d888c153e1405b5a30ed683e135be04a0
linboliao <linboliao123@163.com>
2025-08-31 20:43:18 +0800

    迈新：增加测试数据；修改测试sh 脚本

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,4 +2,6 @@ export PYTHONPATH=../../MIL_BASELINE:$PYTHONPATH
 export LD_LIBRARY_PATH=/home/lbliao/anaconda3/envs/clam/lib:$LD_LIBRARY_PATH
 cd ../
 
-python test_mil.py --yaml_path configs/xiehe/AB_MIL.yaml --test_dataset_csv datasets/xiehe/test.csv --model_weight_path logs/xiehe/AB_MIL/2025-08-14-16-32_seed_2024/fold_1/Best_EPOCH_91.pth --test_log_dir logs/gleason/test
+python test_mil.py --yaml_path configs/cancer/AB_MIL.yaml --test_dataset_csv datasets/cancer/test.csv \
+--model_weight_path result/cancer/0829/AB_MIL/time_2025-08-29-14-36_0829_AB_MIL_seed_42/fold_1/Best_EPOCH_3.pth \
+--test_log_dir result/cancer/test/DS_MIL/fold_1
