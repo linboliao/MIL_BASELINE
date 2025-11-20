87cebc407ad4633a908bee75b80993863bf9d912
Yjing07 <jingy_7@163.com>
2025-11-20 02:10:41 +0000

    协和: 增加pmmr dmmr分类配置

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,55 @@
 export PYTHONPATH=../../MIL_BASELINE:$PYTHONPATH
 export LD_LIBRARY_PATH=/home/lbliao/anaconda3/envs/clam/lib:$LD_LIBRARY_PATH
-cd ../
 
-python test_mil.py --yaml_path configs/xiehe/AB_MIL.yaml --test_dataset_csv datasets/xiehe/test.csv --model_weight_path logs/xiehe/AB_MIL/2025-08-14-16-32_seed_2024/fold_1/Best_EPOCH_91.pth --test_log_dir logs/gleason/test
+config=configs/xiehe/CLAM_SB_MIL.yaml
+test_dataset_csv=datasets/p-dMMR/test.csv
+base_dir=result/p-dMMR/CLAM_SB_MIL/time_2025-11-15-17-51_p-dMMR_CLAM_SB_MIL_seed_42
+gpu=0
+#cd ../
+
+for fold in {1..5}; do
+    model_weight_path="${base_dir}/fold_${fold}/Best_EPOCH_"*.pth
+
+    if [ ! -f ${model_weight_path} ]; then
+        echo "警告: 在 fold_${fold} 未找到 Best_EPOCH_*.pth 文件，跳过。"
+        continue
+    fi
+
+    test_log_dir="${base_dir}/best/fold_${fold}"
+
+    mkdir -p ${test_log_dir}
+
+    echo "正在测试 fold_${fold}，模型: ${model_weight_path}"
+    CUDA_VISIBLE_DEVICES=$gpu python test_mil.py --yaml_path ${config} --test_dataset_csv ${test_dataset_csv} --model_weight_path ${model_weight_path} --test_log_dir ${test_log_dir}
+
+    if [ $? -eq 0 ]; then
+        echo "fold_${fold} 测试完成。"
+    else
+        echo "错误: fold_${fold} 测试失败！"
+    fi
+done
+echo "所有fold测试完毕。"
+
+for fold in {1..5}; do
+    model_weight_path="${base_dir}/fold_${fold}/Last_EPOCH_"*.pth
+
+    if [ ! -f ${model_weight_path} ]; then
+        echo "警告: 在 fold_${fold} 未找到 Last_EPOCH_*.pth 文件，跳过。"
+        continue
+    fi
+
+    test_log_dir="${base_dir}/last/fold_${fold}"
+
+    mkdir -p ${test_log_dir}
+
+    echo "正在测试 fold_${fold}，模型: ${model_weight_path}"
+    CUDA_VISIBLE_DEVICES=$gpu python test_mil.py --yaml_path ${config} --test_dataset_csv ${test_dataset_csv} --model_weight_path ${model_weight_path} --test_log_dir ${test_log_dir}
+
+    if [ $? -eq 0 ]; then
+        echo "fold_${fold} 测试完成。"
+    else
+        echo "错误: fold_${fold} 测试失败！"
+    fi
+done
+echo "所有fold测试完毕。"
+
